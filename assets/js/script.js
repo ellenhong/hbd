3bde7d29770d885eae928f65686d00fdd240f5c0
Ellen Hong <e.hong@berkeley.edu>
2016-05-01 02:26:13 -0700

    added

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+	setTimeout(function () {
+	    $('#hbd1').show().addClass('hbd fadeInUp');}, 50
+	);
+	setTimeout(function () {
+	    $('#hbd2').show().addClass('hbd fadeInUp');}, 1500
+	);
+	setTimeout(function () {
+	    $('#jul').show().addClass('hbd fadeInUp');}, 3000
+	);
+	setTimeout(function () {
+	    $('#text-wrapper').addClass('slideUpAnim');}, 5000
+	);
+})
